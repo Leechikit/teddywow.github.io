b62541f435bebd182887b9c270a7bb5e3f76dbff
teddywow <iteddywow@163.com>
2015-03-19 21:33:07 +0800

    modify resume

--- resume/js/drawStar.js
+++ resume/js/drawStar.js
@@ -1,82 +1,53 @@
-var w = document.documentElement.clientWidth;
-var h = document.documentElement.clientHeight*getPartCount();
-var hh = h >= 300 ? h :300;
+(function(){
+	var SVG_NS = "http://www.w3.org/2000/svg";  
+	var XLINK_NS = "http://www.w3.org/1999/xlink";  
+	  
+	var ATTR_MAP = {  
+	    "className": "class",  
+	    "svgHref": "href"  
+	};  
+	var NS_MAP = {  
+	    "svgHref": XLINK_NS  
+	}; 
+	var w = document.documentElement.clientWidth,
+		h = document.documentElement.clientHeight,
+		hh = h >= 300 ? h :300,
+		svg = document.getElementById("starSvg"),
+		starArr = [];
+	function setSvg(){
+		svg.style.width=w+"px";
+		svg.style.height=hh+"px";
+	}
+	setSvg();
 
-var canvas = document.getElementById("starCanvas");
-canvas.width = w;
-canvas.height = hh;
-var context = canvas.getContext("2d");
-var starArr = [];
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-for (var i = 0; i < 10*getPartCount(); i++) {
-    var starObj = {
-        radius1 : 3+3*Math.random(),
-        radius2 : 1+1*Math.random(),
-        x : 6+(canvas.width-12)*Math.random(),
-        y : 6+(canvas.height-12)*Math.random(),
-        num : Math.ceil(3+2*Math.random()),
-        // color : "rgb("+parseInt(255*Math.random())+","+parseInt(255*Math.random())+","+parseInt(255*Math.random())+")",
-        color : "rgb(255,255,255)",
-        angle : 360*Math.random(),
-        changeAngle : 2
-    }
-    starArr.push(starObj);
-};
-
-function getPartCount(){
-    var lis=document.querySelectorAll('#scroller>section');
-    return lis.length;
-}
-function animate() {
-    context.clearRect(0,0,canvas.width,canvas.height);
-    for (var i = 0; i < starArr.length; i++) {
-        starArr[i].angle+=starArr[i].changeAngle;
-        context.save();
-        context.beginPath();
-        context.translate(starArr[i].x,starArr[i].y);
-        context.rotate(starArr[i].angle*Math.PI/180);
-        context.scale(Math.sin(starArr[i].angle*Math.PI/180),Math.sin(starArr[i].angle*Math.PI/180));
-        //context.globalAlpha = Math.abs(Math.sin(starArr[i].angle*Math.PI/180));
-        drawStar(0,0,starArr[i].radius1,starArr[i].radius2,starArr[i].num,"fill",starArr[i].color);
-        context.restore();
-    };
-    //requestAnimFrame(animate);
-};
-animate();
-
-
-function drawStar(x,y,radius1,radius2,num,drawType,color) {
-    var angle = 360/(num*2);
-    var arr = [];
-    for (var i = 0; i < num*2; i++) {
-        var starObj = {};
-        if (i%2 == 0) {
-            starObj.x = x+radius1*Math.cos(i*angle*Math.PI/180);
-            starObj.y = y+radius1*Math.sin(i*angle*Math.PI/180);
-        }else {
-            starObj.x = x+radius2*Math.cos(i*angle*Math.PI/180);
-            starObj.y = y+radius2*Math.sin(i*angle*Math.PI/180);
-        };
-        arr.push(starObj);
-    }
-    context.beginPath();
-    context.fillStyle = color;
-    context.strokeStyle = color;
-    context.moveTo(arr[0].x,arr[0].y);
-    for (var i = 0; i < arr.length; i++) {
-        context.lineTo(arr[i].x,arr[i].y);
-    };
-    context.closePath();
-    if (drawType == "fill") {
-        context.fill();
-    }else {
-        context.stroke();
-    };
-}
+	for (var i = 0,len=10; i <len ; i++) {
+	    var starObj = {
+	        x : Math.floor(svg.offsetWidth*Math.random()),
+	        y : Math.floor(svg.offsetHeight*Math.random()),
+	        angle : 360*Math.random()
+	    }
+	    starArr.push(starObj);
+	};
+	function makeSVG(tag, attributes){  
+	    var elem = document.createElementNS(SVG_NS, tag);  
+	    for (var attribute in attributes) {  
+	        var name = (attribute in ATTR_MAP ? ATTR_MAP[attribute] : attribute);  
+	        var value = attributes[attribute];  
+	        if (attribute in NS_MAP)   
+	            elem.setAttributeNS(NS_MAP[attribute], name, value);  
+	        else   
+	            elem.setAttribute(name, value);  
+	    }  
+	    return elem;  
+	}  
+	function drawStar(){
+		for(var i=0,len=starArr.length;i<len;i++){
+			var group=makeSVG("g",{transform:"translate("+starArr[i].x+","+starArr[i].y+") scale(0."+Math.round(Math.random()+3)+")"}),
+				starItem=makeSVG("polygon",{points:"9,0 3,17 18,7 0,7 15,17",fill:"#fff"});
+			group.appendChild(starItem);
+			svg.appendChild(group);
+		}
+	}
+	svg.innerHTML="";
+	drawStar();
+})()
